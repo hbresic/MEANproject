0cbdd70fbe28c3f0880ce29aaec21cf3ca90effd
Hrvoje Bresic <bresic.hrvoje@gmail.com>
2015-03-20 23:10:09 +0100

    Implemented secure passwords and signout functionality.

--- public/app/account/mvNavBarLoginCtrl.js
+++ public/app/account/mvNavBarLoginCtrl.js
@@ -1,4 +1,4 @@
-angular.module('app').controller('mvNavBarLoginCtrl', function($scope, $http, mvIdentity, mvNotifier, mvAuth) {
+angular.module('app').controller('mvNavBarLoginCtrl', function($scope, $http, mvIdentity, mvNotifier, mvAuth, $location) {
   $scope.identity = mvIdentity;
   $scope.signin = function(username, password) {
     mvAuth.authenticateUser(username, password).then(function(success) {
@@ -13,6 +13,17 @@ angular.module('app').controller('mvNavBarLoginCtrl', function($scope, $http, mv
         console.log("This code doesn't run {mvNotifier.notify('Username/Password combination incorrect');}");
         alert('Incorrect username or password');
       }
+    });
+  };
+  $scope.signout = function() {
+    mvAuth.logoutUser().then(function() {
+      $scope.username = "";
+      $scope.password = "";
+          /* //Disabled due Toastr/Angular bug
+      mvNotifier.notify('You have been successfully signed out!');*/
+      console.log("This code doesn't run {mvNotifier.notify('You have been successfully signed out!');}");
+      alert('You are signed out!');
+      $location.path('/');
     });
   };
 });
